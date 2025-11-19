3e69f5e0ea1bc21c9d1c1cbdac4a97774a2cda48
Cameron-Wiggins-OU <cameron.j.wiggins-1@ou.edu>
2025-11-18 22:10:00 -0600

    creating LinkedList add(index, data) method

--- src/linkedListPkg/LinkedList.java
+++ src/linkedListPkg/LinkedList.java
@@ -25,6 +25,31 @@ public class LinkedList<T> {
 			
 		}
 	}
+
+	public void add(int index, T data) {
+	    if (index < 0 || index > size) {
+	        throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+	    }
+
+	    if (index == 0) {
+	        Node<T> newNode = new Node<T>(data);
+	        newNode.setNext(startingNode);
+	        startingNode = newNode;
+	        size++;
+	        return;
+	    }
+
+	    Node<T> temp = startingNode;
+	    for (int i = 0; i < index - 1; i++) {
+	        temp = temp.getNext();
+	    }
+
+	    Node<T> newNode = new Node<T>(data);
+	    newNode.setNext(temp.getNext());
+	    temp.setNext(newNode);
+
+	    size++;
+	}
 	
 	public T remove(int index) {
 		Node<T> temp = startingNode;
