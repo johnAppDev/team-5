adf8a1aaf482d4bba7a17d413119813a7732b431
Cameron-Wiggins-OU <cameron.j.wiggins-1@ou.edu>
2025-11-18 22:47:31 -0600

    updated to test custom verison of classes and a better average

--- src/testPkg/InputSizeAddTest.java
+++ src/testPkg/InputSizeAddTest.java
@@ -9,73 +9,61 @@ public class InputSizeAddTest {
     public static void main(String[] args) {
         // Input sizes to test
         int[] sizes = {100, 1000, 3000, 10000, 50000, 100000, 300000, 500000, 1000000};
-        int repetitions = 5; // run each test 5 times to average
+        int repetitions = 100; // run each test 25 times to average
 
-        System.out.println("Java implementations");
-        System.out.printf("%-12s %-12s %-15s%n", "ListType", "Size", "AvgAddTime(ns)");
-        System.out.println("------------------------------------------");
+        System.out.printf("%-18s %-12s %-15s%n", "ListType", "Size", "AvgAddTime(ns)");
+        System.out.println("----------------------------------------------");
 
         for (int size : sizes) {
-            // Test ArrayList
+            // Test Java ArrayList
             long arrayListTotal = 0;
             for (int r = 0; r < repetitions; r++) {
-                List<Integer> arrayList = new ArrayList<>();
+            	java.util.ArrayList<Integer> arrayList = new java.util.ArrayList<>();
                 long start = System.nanoTime();
                 for (int i = 0; i < size; i++) arrayList.add(i);
                 long end = System.nanoTime();
                 arrayListTotal += (end - start);
             }
             long arrayListAvg = arrayListTotal / repetitions;
+            
+            // Test Custom ArrayList
+            long customArrayListTotal = 0;
+            for (int r = 0; r < repetitions; r++) {
+            	arrayListPkg.ArrayList<Integer> customArrayList = new arrayListPkg.ArrayList<>();
+                long start = System.nanoTime();
+                for (int i = 0; i < size; i++) customArrayList.add(i);
+                long end = System.nanoTime();
+                customArrayListTotal += (end - start);
+            }
+            long customArrayListAvg = customArrayListTotal / repetitions;
 
-            // Test LinkedList
+            // Test Java LinkedList
             long linkedListTotal = 0;
             for (int r = 0; r < repetitions; r++) {
-                List<Integer> linkedList = new LinkedList<>();
+            	java.util.LinkedList<Integer> linkedList = new java.util.LinkedList<>();
                 long start = System.nanoTime();
                 for (int i = 0; i < size; i++) linkedList.add(i);
                 long end = System.nanoTime();
                 linkedListTotal += (end - start);
             }
             long linkedListAvg = linkedListTotal / repetitions;
+            
+            // Test Custom LinkedList
+            long customLinkedListTotal = 0;
+            for (int r = 0; r < repetitions; r++) {
+            	linkedListPkg.LinkedList<Integer> customLinkedList = new linkedListPkg.LinkedList<>();
+                long start = System.nanoTime();
+                for (int i = 0; i < size; i++) customLinkedList.add(i);
+                long end = System.nanoTime();
+                customLinkedListTotal += (end - start);
+            }
+            long customLinkedListAvg = customLinkedListTotal / repetitions;
 
             // Print results
-            System.out.printf("%-12s %-12d %-15d%n", "ArrayList", size, arrayListAvg);
-            System.out.printf("%-12s %-12d %-15d%n", "LinkedList", size, linkedListAvg);
-            
-            
-            arrayListPkg.ArrayList<Integer> costumList = new arrayListPkg.ArrayList<Integer>();
-            System.out.println(" Costum implementations");
-            System.out.printf("%-12s %-12s %-15s%n", "ListType", "Size", "AvgAddTime(ns)");
-            System.out.println("------------------------------------------");
-
-            for (int size1 : sizes) {
-                // Test ArrayList
-                long arrayListTotal1 = 0;
-                for (int r = 0; r < repetitions; r++) {
-                    List<Integer> arrayList = new ArrayList<>();
-                    long start = System.nanoTime();
-                    for (int i = 0; i < size1; i++) arrayList.add(i);
-                    long end = System.nanoTime();
-                    arrayListTotal1 += (end - start);
-                }
-                long arrayListAvg1 = arrayListTotal1 / repetitions;
-
-                // Test LinkedList
-                long linkedListTotal1 = 0;
-                for (int r = 0; r < repetitions; r++) {
-                    List<Integer> linkedList = new LinkedList<>();
-                    long start = System.nanoTime();
-                    for (int i = 0; i < size1; i++) linkedList.add(i);
-                    long end = System.nanoTime();
-                    linkedListTotal1 += (end - start);
-                }
-                long linkedListAvg1 = linkedListTotal1 / repetitions;
-
-                // Print results
-                System.out.printf("%-12s %-12d %-15d%n", "ArrayList", size1, arrayListAvg1);
-                System.out.printf("%-12s %-12d %-15d%n", "LinkedList", size1, linkedListAvg1);
+            System.out.printf("%-18s %-12d %-15d%n", "Java ArrayList", size, arrayListAvg);
+            System.out.printf("%-18s %-12d %-15d%n", "Custom ArrayList", size, customArrayListAvg);
+            System.out.printf("%-18s %-12d %-15d%n", "Java LinkedList", size, linkedListAvg);
+            System.out.printf("%-18s %-12d %-15d%n", "Custom LinkedList", size, customLinkedListAvg);
         }
-        
-    }
     }
 }
